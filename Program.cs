3afafb5651e7c85840eb323f831e8ae0b54e7a74
Jose Velasquez <velasquezdenorajose@gmail.com>
2025-06-02 16:30:56 -0400

    🧑‍💻 Remove namespace & class for better readability

--- Program.cs
+++ Program.cs
@@ -1,149 +1,135 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿List<string> TodoList = new List<string>();
 
-namespace ToDo
+Menu[] validOptions = [Menu.Add, Menu.Remove, Menu.List, Menu.Exit];
+Menu menuOption;
+do
 {
-    internal class Program
+    menuOption = (Menu)ShowMainMenu();
+    if (menuOption == Menu.Add)
     {
-        public static List<string> TodoList { get; set; } = new List<string>();
+        ShowMenuAdd();
+    }
+    else if (menuOption == Menu.Remove)
+    {
+        ShowMenuRemove();
+    }
+    else if (menuOption == Menu.List)
+    {
+        ShowMenuTodoList();
+    }
+    else if (!validOptions.Contains(menuOption))
+    {
+        Console.WriteLine("Invalid option, try again");
+    }
+} while (menuOption != Menu.Exit);
 
-        static void Main(string[] args)
+int ShowMainMenu()
+{
+    Console.WriteLine("----------------------------------------");
+    Console.WriteLine("Enter the option to be made: ");
+    Console.WriteLine("1. New task");
+    Console.WriteLine("2. Remove task");
+    Console.WriteLine("3. Show pending tasks");
+    Console.WriteLine("4. Exit");
+    Console.WriteLine("----------------------------------------");
+
+    // Read line
+    string option = Console.ReadLine();
+    Console.WriteLine("----------------------------------------");
+    return Convert.ToInt32(option);
+}
+
+void ShowMenuRemove()
+{
+    try
+    {
+        Console.WriteLine("Enter the number of the task to be removed: ");
+        // Show current taks
+        bool isPending = ShowTodoList();
+        if (!isPending) return;
+        Console.WriteLine("----------------------------------------");
+
+        string taskToBeRemoved = Console.ReadLine();
+        // Remove one position
+        int indexToRemove = Convert.ToInt32(taskToBeRemoved) - 1;
+
+        if (indexToRemove <= (TodoList.Count - 1) && indexToRemove > -1 && TodoList.Count > 0)
         {
-            Menu[] validOptions = [Menu.Add, Menu.Remove, Menu.List, Menu.Exit];
-            Menu menuOption;
-            do
-            {
-                menuOption = (Menu)ShowMainMenu();
-                if (menuOption == Menu.Add)
-                {
-                    ShowMenuAdd();
-                }
-                else if (menuOption == Menu.Remove)
-                {
-                    ShowMenuRemove();
-                }
-                else if (menuOption == Menu.List)
-                {
-                    ShowMenuTodoList();
-                }
-                else if (!validOptions.Contains(menuOption))
-                {
-                    Console.WriteLine("Invalid option, try again");
-                }
-            } while (menuOption != Menu.Exit);
-        }
-        /// <summary>
-        /// Show the main menu 
-        /// </summary>
-        /// <returns>Returns option indicated by user</returns>
-        public static int ShowMainMenu()
-        {
+            string task = TodoList[indexToRemove];
+            TodoList.RemoveAt(indexToRemove);
             Console.WriteLine("----------------------------------------");
-            Console.WriteLine("Enter the option to be made: ");
-            Console.WriteLine("1. New task");
-            Console.WriteLine("2. Remove task");
-            Console.WriteLine("3. Show pending tasks");
-            Console.WriteLine("4. Exit");
-            Console.WriteLine("----------------------------------------");
-
-            // Read line
-            string option = Console.ReadLine();
-            Console.WriteLine("----------------------------------------");
-            return Convert.ToInt32(option);
+            Console.WriteLine($"Task {task} removed");
         }
-
-        public static void ShowMenuRemove()
+        else
         {
-            try
-            {
-                Console.WriteLine("Enter the number of the task to be removed: ");
-                // Show current taks
-                bool isPending = ShowTodoList();
-                if (!isPending) return;
-                Console.WriteLine("----------------------------------------");
-
-                string taskToBeRemoved = Console.ReadLine();
-                // Remove one position
-                int indexToRemove = Convert.ToInt32(taskToBeRemoved) - 1;
-
-                if (indexToRemove <= (TodoList.Count - 1) && indexToRemove > -1 && TodoList.Count > 0)
-                {
-                    string task = TodoList[indexToRemove];
-                    TodoList.RemoveAt(indexToRemove);
-                    Console.WriteLine("----------------------------------------");
-                    Console.WriteLine($"Task {task} removed");
-                }
-                else
-                {
-                    Console.WriteLine("Task not found");
-                }
-            }
-            catch (Exception)
-            {
-                Console.WriteLine("----------------------------------------");
-                Console.WriteLine("Something went wrong, try again");
-            }
-        }
-
-        public static void ShowMenuAdd()
-        {
-            try
-            {
-                Console.WriteLine("Enter a new task: ");
-                string task = Console.ReadLine();
-
-                if (!string.IsNullOrEmpty(task))
-                {
-                    TodoList.Add(task);
-                    Console.WriteLine("----------------------------------------");
-                    Console.WriteLine("Task registered");
-                }
-                else
-                {
-                    Console.WriteLine("Please enter a valid task");
-                }
-
-            }
-            catch (Exception)
-            {
-                Console.WriteLine("----------------------------------------");
-                Console.WriteLine("Something went wrong, try again");
-            }
-
-        }
-
-        public static void ShowMenuTodoList()
-        {
-            ShowTodoList();
-        }
-
-        public static bool ShowTodoList()
-        {
-            if (TodoList?.Count > 0)
-            {
-                Console.WriteLine("Your pending tasks:");
-
-                int taskIndex = 1;
-                TodoList.ForEach(task =>
-                {
-                    Console.WriteLine($"{taskIndex++}. {task}");
-                });
-                return true;
-            }
-            else
-            {
-                Console.WriteLine("No pending tasks");
-                return false;
-            }
-        }
-
-        public enum Menu
-        {
-            Add = 1,
-            Remove = 2,
-            List = 3,
-            Exit = 4
+            Console.WriteLine("Task not found");
         }
     }
+    catch (Exception)
+    {
+        Console.WriteLine("----------------------------------------");
+        Console.WriteLine("Something went wrong, try again");
+    }
 }
+
+void ShowMenuAdd()
+{
+    try
+    {
+        Console.WriteLine("Enter a new task: ");
+        string task = Console.ReadLine();
+
+        if (!string.IsNullOrEmpty(task))
+        {
+            TodoList.Add(task);
+            Console.WriteLine("----------------------------------------");
+            Console.WriteLine("Task registered");
+        }
+        else
+        {
+            Console.WriteLine("Please enter a valid task");
+        }
+
+    }
+    catch (Exception)
+    {
+        Console.WriteLine("----------------------------------------");
+        Console.WriteLine("Something went wrong, try again");
+    }
+
+}
+
+void ShowMenuTodoList()
+{
+    ShowTodoList();
+}
+
+bool ShowTodoList()
+{
+    if (TodoList?.Count > 0)
+    {
+        Console.WriteLine("Your pending tasks:");
+
+        int taskIndex = 1;
+        TodoList.ForEach(task =>
+        {
+            Console.WriteLine($"{taskIndex++}. {task}");
+        });
+        return true;
+    }
+    else
+    {
+        Console.WriteLine("No pending tasks");
+        return false;
+    }
+}
+
+enum Menu
+{
+    Add = 1,
+    Remove = 2,
+    List = 3,
+    Exit = 4
+}
+
+
